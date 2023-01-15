340c143e93549bf8184e3b460c2915ea241d65d9
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-15 09:57:32 +0300

    Pass data down from App to Tasklist. Display list of tasks in Tasklist using the Task component.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks}) {
+  
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map((item, index) => (
+        <Task key={item.id} name={item.text} category={item.category} />
+      ))}
     </div>
   );
 }
